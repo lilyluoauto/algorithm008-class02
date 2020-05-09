76c0551fc5a4430a70b1da801df34189f5236f4b
lilyluo <licy_luo@163.com>
2020-05-09 19:32:09 +0800

    0.1

---
+++ Week_04/BFSTemp.py
@@ -0,0 +1,15 @@
+def BFS(graph, start, end):
+    visited = set()
+	queue = []
+	queue.append([start])
+
+	while queue:
+		node = queue.pop()
+		visited.add(node)
+
+		process(node)
+		nodes = generate_related_nodes(node)
+		queue.push(nodes)
+
+	# other processing work
+	...
